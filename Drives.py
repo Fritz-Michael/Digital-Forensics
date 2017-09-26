d04012172dff871aebb77a815edf51511c952ca4
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-09-26 21:54:02 +0800

    first commit

---
+++ Drives.py
@@ -0,0 +1,23 @@
+import string
+from ctypes import windll
+import time
+import os
+
+def get_drives():
+    drives = []
+    bitmask = windll.kernel32.GetLogicalDrives()
+    for letter in string.uppercase:
+        if bitmask & 1:
+            drives.append(letter)
+        bitmask >>= 1
+    return drives
+
+
+if __name__ == '__main__':
+	drives = set(get_drives())
+	x = 0
+	for drive in drives:
+		print("[%d] %s" % (x,drive))
+		x += 1
+	#option = raw_input("Choose a System Drive: ")
+	
