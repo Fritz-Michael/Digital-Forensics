47b1baee9e472582e257f6c7fcbf4ef3aef2526e
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-10-01 16:17:47 +0800

    looking for master boot

--- WindowsFunctions.py
+++ WindowsFunctions.py
@@ -46,41 +46,46 @@ def readdriveWin(path, rootPath):
 	sectorPerCluster = getsectorspercluster(rootPath)
 
 	sectorNo = 0
-	sectorMax = 10000000
+	sectorMax = 100000000
 	prev = '0'
 	cur = '0'
 	imagectr = 0
 	maxSize = 10000000
-
 	while sectorNo < sectorMax:
-		try:
-			drive.seek(sectorNo * bytesPerSector) # iterates per sector
-			cur = reader = drive.read(1) # reads the first byte of each sector
+		data = drive.read(1)
+		print(data)
+		prev = cur
 
-			if cur == b'\xFD':
-				nextbyte = drive.read(1) # reads the second byte of each sector
-				if nextbyte == b'\xD8': # will be true if a JPG file header, FFD8, is detected
-					print("FOUND - ", sectorNo)
-					imagectr += 1 # iterate to designate the image number
-					image = open("found\\" + str(imagectr) + ".jpg","wb") # creates a new file in the 'found' folder and allows to write in bytes
-					print("hello")
-					running = True # used to designate that the writer is running
-					image.write(b'\xFF') # writes the JPG headers to the new file
-					image.write(b'\xD8')
-					mCtr = 0 # mCtr is used as a limiter just in case the new file is not really a JPG file, it stops at 10Mb, can be removed if image files is larger
-					while running and mCtr < maxSize: # loops until the footer FFD9 is detected or arbitrary limit is reached
-						cur = reader = drive.read(1) # reads one byte at a time from the possible JPG file
-						image.write(cur) # write the byte from the JPG file read
-						if cur == b'\xD9' and prev == b'\xFF': # responsible for checking if the footer FFD9 is found
-							running = False # once the footer is found, running will be set to false to end the loop
-							image.close() # the file would be closed to save the image
-							print("Image Saved")
-						prev = cur # sets the cur value to prev, this is used to detect the 2 bytes for FF and D9
-						mCtr += 1 # this counter is used to iterate until it reaches the arbitrary limit
+	# while sectorNo < sectorMax:
+	# 	try:
+	# 		drive.seek(sectorNo * bytesPerSector) # iterates per sector
+	# 		cur = reader = drive.read(1) # reads the first byte of each sector
+	# 		print(cur)
+	# 		if cur == b'\xFD':
+	# 			nextbyte = drive.read(1) # reads the second byte of each sector
+	# 			if nextbyte == b'\xD8': # will be true if a JPG file header, FFD8, is detected
+	# 				print("FOUND - ", sectorNo)
+	# 				imagectr += 1 # iterate to designate the image number
+	# 				image = open("found\\" + str(imagectr) + ".jpg","wb") # creates a new file in the 'found' folder and allows to write in bytes
+	# 				print("hello")
+	# 				running = True # used to designate that the writer is running
+	# 				image.write(b'\xFF') # writes the JPG headers to the new file
+	# 				image.write(b'\xD8')
+	# 				mCtr = 0 # mCtr is used as a limiter just in case the new file is not really a JPG file, it stops at 10Mb, can be removed if image files is larger
+	# 				while running and mCtr < maxSize: # loops until the footer FFD9 is detected or arbitrary limit is reached
+	# 					cur = reader = drive.read(1) # reads one byte at a time from the possible JPG file
+	# 					image.write(cur) # write the byte from the JPG file read
+	# 					if cur == b'\xD9' and prev == b'\xFF': # responsible for checking if the footer FFD9 is found
+	# 						running = False # once the footer is found, running will be set to false to end the loop
+	# 						image.close() # the file would be closed to save the image
+	# 						print("Image Saved")
+	# 						raw_input("pause")
+	# 					prev = cur # sets the cur value to prev, this is used to detect the 2 bytes for FF and D9
+	# 					mCtr += 1 # this counter is used to iterate until it reaches the arbitrary limit
 
-				if mCtr >= maxSize: # just in case the arbitrary value is reached, it would safely close the image
-					image.close()
-					print("Image Saved - failed")
-		except:
-			pass
-		sectorNo += 1
+	# 			if mCtr >= maxSize: # just in case the arbitrary value is reached, it would safely close the image
+	# 				image.close()
+	# 				print("Image Saved - failed")
+	# 	except:
+	# 		pass
+	# 	sectorNo += 1
