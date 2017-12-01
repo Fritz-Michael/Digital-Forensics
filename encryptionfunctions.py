60c324e75061d71b21c41f9f2bdf9a69f998ea2e
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-12-01 14:30:21 +0800

    next time

--- encryptionfunctions.py
+++ encryptionfunctions.py
@@ -10,15 +10,42 @@ def reverse_message(message):
 def generate_key(reversed):
 	return rand.randint(len(reversed),max(reversed))
 
-def increment_bits(reversed,increment):
-	return list(map(lambda x: ((x+increment)%57)+65,reversed))
+def increment_message(reversed,key):
+	return list(map(lambda x: ((x+key)%57)+66,reversed))
+
+def reverse_to_char(message):
+	temp = message[::-1]
+	return list(map(lambda x: chr(x), temp))
+
+def to_ascii(message):
+	return [ord(x) for x in message]
+
+def decrement_message(message, key):
+	temp = list(map(lambda x: x-65,message))
+	return list(map(lambda x: (x-key)%57,temp))
 
 def encrypt_message(message):
 	reverse = reverse_message(message)
 	key = generate_key(reverse)
-	print(key)
-	inc_list = increment_bits(reverse,key)
+	inc_list = increment_message(reverse,key)
 	return (list(map(lambda x: chr(x), inc_list)),key)
 
+def decrypt_message(message, key):
+	message = to_ascii(message)
+	dec_list = decrement_message(message,key)
+	return reverse_to_char(dec_list)
+
+
+
 if __name__ == '__main__':
-	print(''.join(encrypt_message('hello world')[0]))
+	temp = encrypt_message('hello world')
+	print(temp)
+	# print(decrypt_message(temp[0],temp[1]))
+	#print(''.join(encrypt_message('hello world')[0]))
+	# b = 29
+	# a = 16
+	# x = (a + b)%29
+	# print(x)
+	# b = ((x - a)*-1)%29
+	# print(b)
+	#print(bin(10))
