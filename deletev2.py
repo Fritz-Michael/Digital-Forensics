13dfff083628d195bb86ee05e89aba808ef4a80d
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-12-05 13:50:27 +0800

    update

--- deletev2.py
+++ deletev2.py
@@ -51,28 +51,59 @@ def mftlocation(path, rootPath):
     drive.read(48)
     location = int.from_bytes(drive.read(8),byteorder='little')
     location *= getsectorspercluster(rootPath)
-    location * getbytespersector(rootPath)
+    location *= getbytespersector(rootPath)
     drive.close()
     return location
 
+def getfiles(path, rootpath):
+    drive = open(path, 'rb')
+    sector = mftlocation(path, rootpath) / getbytespersector(rootpath) + 10
+    bytepos = int(getbytespersector(rootpath) * sector)
+    flist = []
+    data = getdloc(bytepos, path, rootpath)
+    flist.append({'is_folder': data["is_folder"],'folder_name': data["file_name"], 'record_number': data["MFT_rec"], 'parent_directory': data["parent_dir"], 'dir_path': rootpath + ":", 'num_child': 0, 'list_child': []})
+    sector = sector + 68
+    bytepos = int(getbytespersector(rootpath) * sector)
+    ifmft = True
+
+    while ifmft:
+        drive.seek(bytepos)
+        if drive.read(4).decode('utf-8') == 'FILE':
+            data = getdloc(bytepos, path, rootpath)
+            if data["is_folder"]:
+                for directory in flist:
+                    if directory["record_number"] == data["parent_dir"]:
+                        file_path = directory["dir_path"] + "\\" + data["file_name"]
+                        directory["num_child"] += 1
+                flist.append({'is_folder': data["is_folder"],'folder_name': data["file_name"], 'record_number': data["MFT_rec"], 'parent_directory': data["parent_dir"], 'dir_path': file_path, 'num_child': 0, 'list_child': []})             
+            else:
+                for directory in flist:
+                    if directory["record_number"] == data["parent_dir"]:
+                        data["dir_path"] = directory["dir_path"] + '\\' + data["file_name"]
+                        directory["num_child"] += 1
+                        directory["list_child"].append(data)
+            
+        elif drive.read(4).decode('utf-8') == 'BAAD':
+            flist.append(None)
+        else:
+            ifmft = False
+        sector = sector + 2
+        bytepos = int(getbytespersector(rootpath) * sector)
+    drive.close()
+    return flist
+
 def getdloc(offset, path, rootpath): # change rootpath later for byte position of MFT record 
     drive = open(path, 'rb')
-    multidrun = False
-    mdrun = []
-    ### skip initial system entry ###
-    ### getting position place in driver ###
-    
+    multidrun = False   
+    mdrun = []    
     currboff = offset
     currMFTrec = currboff
     drive.seek(currboff)
     drive.read(20) ### read 20 ###
     nattroff = int.from_bytes(drive.read(2),byteorder='little') #next attribute from file header
     curattrhead = b'00000000'
-
     mftstatus = binascii.hexlify(drive.read(2))
-
     drive.seek(20, 1)
-
     recnum = int.from_bytes(drive.read(4),byteorder='little')
 
     if mftstatus == b'0000':
@@ -90,16 +121,11 @@ def getdloc(offset, path, rootpath): # change rootpath later for byte position o
         currboff = drive.tell()
         curattrhead = binascii.hexlify(drive.read(4)) #atrib type
         nattroff = int.from_bytes(drive.read(4),byteorder='little') #attrib size
-
     drive.seek(16, 1)
-
     pdir = int.from_bytes(drive.read(6),byteorder='little')
-    
     drive.seek(58, 1)
     fsize = int.from_bytes(drive.read(1),byteorder='little')
-
     drive.read(1)
-    
     fname = drive.read(2 * fsize).decode('utf-16')
 
     if fname.find("~",0, fsize) != -1:
@@ -109,20 +135,15 @@ def getdloc(offset, path, rootpath): # change rootpath later for byte position o
         curattrhead = binascii.hexlify(drive.read(4)) #atrib type
         nattroff = int.from_bytes(drive.read(4),byteorder='little') #attrib size
         drive.seek(16, 1)
-
         pdir = int.from_bytes(drive.read(6),byteorder='little')
-        
         drive.seek(58, 1)
         fsize = int.from_bytes(drive.read(1),byteorder='little')
-
         drive.read(1)
-        
         fname = drive.read(2 * fsize).decode('utf-16')
     
     if isfolder:
         drive.close()
         return {'is_folder': True, 'file_name': fname, 'parent_dir': pdir, 'MFT_rec': recnum}
-
 
     while curattrhead != b'80000000':
         drive.seek(currboff)
@@ -130,9 +151,10 @@ def getdloc(offset, path, rootpath): # change rootpath later for byte position o
         currboff = drive.tell()
         curattrhead = binascii.hexlify(drive.read(4)) #atrib type
         nattroff = int.from_bytes(drive.read(4),byteorder='little') #attrib size
-        nrflag = int.from_bytes(drive.read(1),byteorder='little') # nr or r
+        nrflag = int.from_bytes(drive.read(1),byteorder='little') # nr or r    
     
     drive.seek(currboff)
+
     if nrflag:
         drive.seek(32, 1) #offset to data run offset
         drunoff = int.from_bytes(drive.read(2),byteorder='little') #data run offset
@@ -145,7 +167,7 @@ def getdloc(offset, path, rootpath): # change rootpath later for byte position o
         if binascii.hexlify(drive.read(1)) == b'00':
             drive.close()
             return {'is_folder': False, 'file_name': fname, 'parent_dir': pdir, 'MFT_rec': recnum, 'multi_run': False, 'data_start': drstart, 'data_totalsize': clustsize}
-            #delete(path, drstart, clustsize)
+
         else:
             drive.seek(-1, 1)
             mdrun.append((drstart, clustsize))
@@ -157,12 +179,9 @@ def getdloc(offset, path, rootpath): # change rootpath later for byte position o
                 drstart = int.from_bytes(drive.read(high),byteorder='little') * getsectorspercluster(rootpath) * getbytespersector(rootpath)
                 mdrun.append((drstart, clustsize))
 
-            #delete(path, mdrun, multi=True)
             drive.close()
             return {'is_folder': False, 'file_name': fname, 'parent_dir': pdir, 'MFT_rec': recnum, 'multi_run': True, 'data_start': mdrun, 'data_totalsize': clustsize}
-        # drive.seek(drstart)
-        # call function for deletion - pass, byte position, clustersize/ data size, MFT entry position
-        # return drstart, clustsize, currboff
+
     else:
         drive.seek(16, 1)
         dlen = int.from_bytes(drive.read(4),byteorder='little')
@@ -171,14 +190,6 @@ def getdloc(offset, path, rootpath): # change rootpath later for byte position o
         dstart = drive.tell()
         drive.close()
         return {'is_folder': False, 'file_name': fname, 'parent_dir': pdir, 'MFT_rec': recnum, 'multi_run': False, 'data_start': dstart, 'data_totalsize': dlen}
-        # smdata = drive.read(dlen).decode('utf-8')
-        delete(path, drive.tell(), dlen)
-    updatestatus(currMFTrec)
-    drive.close()
-        # call fucntion for deletion - pass byte position, length of data, MFT entry position
-    ### return start MFT file post, position of data blocks, number of clusters ###
-    
-
 
 def deletion(isfolder, datarun, datasize, method, fpath,  npass=0):
     drive = open(fpath,'r+b')
@@ -234,116 +245,11 @@ def deletion(isfolder, datarun, datasize, method, fpath,  npass=0):
     os.remove(fpath)
 
 if __name__ == '__main__':
-    # drive = open('\\\\.\\H:', 'rb')
     dirs = []
 
-    currsec = mftlocation('\\\\.\\H:', 'H')
-    currsec = 2097284
-    currsec = 2097162
+    dirs = getfiles('\\\\.\\H:', 'H')
 
-    while currsec != 0:
-        currboff = currsec * getbytespersector('H')
-        here = getdloc(currboff,'\\\\.\\H:', 'H')
-
-        here["start_sector"] = currsec
-        here["end_sector"] = currsec + 1
-
-        if here["is_folder"]:
-            rootpath = "H:"
-            if here["MFT_rec"] == here["parent_dir"]:
-                root = here["MFT_rec"]
-                path = rootpath
-            else:
-                for directory in dirs:
-                    if directory["record_number"] == here["parent_dir"]:
-                        path = directory["dir_path"] + "\\" + here["file_name"]
-                        directory["num_child"] += 1
-            
-            dirs.append({'is_folder': here["is_folder"],'folder_name': here["file_name"], 'record_number': here["MFT_rec"], 'parent_directory': here["parent_dir"], 'dir_path': path, 'num_child': 0, 'list_child': []})
-
-
-
-        else:
-            for directory in dirs:
-                if directory["record_number"] == here["parent_dir"]:
-                    here["dir_path"] = directory["dir_path"] + "\\" + here["file_name"]
-                    directory["num_child"] += 1
-                    directory["list_child"].append(here)
-
-
-
-        print("Record Number:",here["MFT_rec"], "\n")
-        print("Is Folder:",here["is_folder"], "\n")
-
-        if here["is_folder"]:
-            for x in dirs:
-                if here["MFT_rec"] == x["record_number"]:
-                    print("Folder name:",x["folder_name"], "\n")
-                    print("Folder MFT record number:",x["record_number"], "\n")
-                    print("File path:",x["dir_path"], "\n")
-                    print("Number of files:", x["num_child"], "\n")
-        else:
-            for x in dirs:
-                for y in x["list_child"]:
-                    print("Child path:", y["dir_path"], "\n")
-                    print("Child name:", y["file_name"], "\n")
-                    print("Child parent:", y["parent_dir"], "\n")
-                    print("Child size:", y["data_totalsize"], "\n")
-                    print("Child start:", y["data_start"], "\n")
-
-        currsec = input("Enter number:")
-        currsec = int(currsec)
-
-        print("----------------------------------")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #deletion part
-
-    # for fold in dirs:
-    #   if fold["record_number"] == here["parent_dir"]:
-    #       here{'file_path': fold["dirpath"] + "\\" + here["file_name"]}
-
-    # open(here["file_path"], 'r+b')
-
-
-    # secure erase
-    #   1 pass: one or zero (use random)
-    # zero fill
-    #   1 pass: zero only
-    # schneier
-    #   7 passes:
-    #       Pass 1: Writes a one
-    #       Pass 2: Writes a zero
-    #       Pass 3: Writes a stream of random characters
-    #       Pass 4: Writes a stream of random characters
-    #       Pass 5: Writes a stream of random characters
-    #       Pass 6: Writes a stream of random characters
-    #       Pass 7: Writes a stream of random characters
-    # random data
-    #   x passes: random characters
-    
+    for wew in dirs:
+        print(wew["dir_path"])
+        for wat in wew["list_child"]:
+            print(wat["dir_path"])
